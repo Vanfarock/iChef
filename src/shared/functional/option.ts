1c003f8a9635da1d4680342478f9726fb26b8a9f
Vinícius Manuel Martins <vinicius.m.m.2002@gmail.com>
2022-08-11 17:11:42 -0300

    Finish Option base implementation

--- src/shared/functional/option.ts
+++ src/shared/functional/option.ts
@@ -1,6 +1,6 @@
 export interface Match<T, U> {
   some: (val: T) => U
-  none: (() => U) | U
+  none: () => U
 }
 
 export interface Option<T> {
@@ -11,10 +11,54 @@ export interface Option<T> {
   unwrap(): T | never
 }
 
-export interface OptionSome<T> {
+export interface OptionSome<T> extends Option<T> {
   unwrap(): T
 }
 
-export interface OptionNone<T> {
+export interface OptionNone<T> extends Option<T> {
   unwrap(): never
+}
+
+export function Some<T>(val: T): Option<T> {
+  return someConstructor<T>(val)
+}
+
+function someConstructor<T>(val: T): OptionSome<T> {
+  return {
+    isSome(): boolean {
+      return true
+    },
+    isNone(): boolean {
+      return false
+    },
+    match<U>(fn: Match<T, U>): U {
+      return fn.some(val)
+    },
+    unwrapOr(other: T): T {
+      return val
+    },
+    unwrap() {
+      return val
+    },
+  }
+}
+
+function noneConstructor<T>(): OptionNone<T> {
+  return {
+    isSome(): boolean {
+      return false
+    },
+    isNone(): boolean {
+      return true
+    },
+    match<U>(fn: Match<T, U>): U {
+      return fn.none()
+    },
+    unwrapOr(other: T): T {
+      return other
+    },
+    unwrap() {
+      throw new ReferenceError('Trying to unwrap None.');
+    },
+  }
 }
