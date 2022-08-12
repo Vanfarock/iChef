7846dc5f8ec57ba6635c0f718ae33afddeae4b29
Vinícius Manuel Martins <vinicius.m.m.2002@gmail.com>
2022-08-12 16:42:53 -0300

    Add test to verify if AddItem handles menu section correctly

--- src/application/usecases/restaurant/add-item.spec.ts
+++ src/application/usecases/restaurant/add-item.spec.ts
@@ -1,5 +1,5 @@
 import {
-  Either, right,
+  Either, left, right,
 } from '@functional/either'
 import MenuItem from '@entities/restaurant/menuItem'
 
@@ -21,56 +21,126 @@ type AddItemRequest = {
 }
 
 export class AddItem {
-  constructor(readonly menuItemRepository: MenuItemRepository) {}
+  constructor(
+    readonly menuItemRepository: MenuItemRepository,
+    readonly menuSectionRepository: MenuSectionRepository,
+  ) {}
 
   async execute({ menuId, menuSectionId }: AddItemRequest): Promise<Either<AddItemError, void>> {
-    this.menuItemRepository.add(MenuItem.create({
+    const item = MenuItem.create({
       menuId, menuSectionId,
-    }))
+    })
+
+    if (menuSectionId) {
+      const comparedMenuId = await this.menuSectionRepository.getMenu(menuSectionId)
+
+      if (comparedMenuId !== menuId) {
+        return left(new AddItemError('Menu section is from a different menu.'))
+      }
+
+      await this.menuSectionRepository.add(item)
+    }
+
+    await this.menuItemRepository.add(item)
 
     return right(undefined)
   }
 }
 
 export interface MenuItemRepository {
-  add(item: MenuItem): Promise<Either<AddItemError, void>>;
+  add(item: MenuItem): Promise<void>;
 }
 
 class MenuItemRepositorySpy implements MenuItemRepository {
   items: MenuItem[] = []
   callsCount = 0
 
-  async add(item: MenuItem): Promise<Either<AddItemError, void>> {
+  async add(item: MenuItem): Promise<void> {
     this.items.push(item)
     this.callsCount += 1
+  }
+}
 
-    return right(undefined)
+export interface MenuSectionRepository {
+  getMenu(menuSectionId: string): Promise<string>
+  add(item: MenuItem): Promise<void>
+}
+
+class MenuSectionRepositorySpy implements MenuSectionRepository {
+  items: MenuItem[] = []
+  getMenuCallsCount = 0
+  addCallsCount = 0
+
+  async getMenu(menuSectionId: string): Promise<string> {
+    this.getMenuCallsCount += 1
+
+    return 'menu_id'
+  }
+
+  async add(item: MenuItem): Promise<void> {
+    this.items.push(item)
+    this.addCallsCount += 1
   }
 }
 
 type SutOutput = {
   sut: AddItem,
   menuItemRepository: MenuItemRepositorySpy,
+  menuSectionRepository: MenuSectionRepositorySpy,
 }
 
 const makeSut = (): SutOutput => {
   const menuItemRepository = new MenuItemRepositorySpy()
-  const sut = new AddItem(menuItemRepository)
+  const menuSectionRepository = new MenuSectionRepositorySpy()
+  const sut = new AddItem(menuItemRepository, menuSectionRepository)
 
   return {
     sut,
     menuItemRepository,
+    menuSectionRepository,
   }
 }
 
 describe('AddItem', () => {
   it('should add new item to menu', async () => {
-    const { sut, menuItemRepository } = makeSut()
+    const { sut, menuItemRepository, menuSectionRepository } = makeSut()
 
-    await sut.execute({ menuId: 'menu_id' })
+    const result = await sut.execute({ menuId: 'menu_id' })
 
+    expect(result.isRight()).toBeTruthy()
     expect(menuItemRepository.items).toHaveLength(1)
     expect(menuItemRepository.callsCount).toBe(1)
     expect(menuItemRepository.items[0].menuId()).toEqual('menu_id')
+    expect(menuSectionRepository.items).toHaveLength(0)
+    expect(menuSectionRepository.addCallsCount).toBe(0)
+    expect(menuSectionRepository.getMenuCallsCount).toBe(0)
+  })
+
+  it('should add new item to menu in specific section', async () => {
+    const { sut, menuItemRepository, menuSectionRepository } = makeSut()
+
+    const result = await sut.execute({ menuId: 'menu_id', menuSectionId: 'menu_section_id' })
+
+    expect(result.isRight()).toBeTruthy()
+    expect(menuItemRepository.items).toHaveLength(1)
+    expect(menuItemRepository.callsCount).toBe(1)
+    expect(menuItemRepository.items[0].menuId()).toEqual('menu_id')
+    expect(menuSectionRepository.items).toHaveLength(1)
+    expect(menuSectionRepository.addCallsCount).toBe(1)
+    expect(menuSectionRepository.getMenuCallsCount).toBe(1)
+  })
+
+  it('should return an error when menu section is from a different menu', async () => {
+    const { sut, menuItemRepository, menuSectionRepository } = makeSut()
+
+    const result = await sut.execute({ menuId: 'wrong_menu_id', menuSectionId: 'menu_section_id' })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.left().unwrap().message).toEqual('Menu section is from a different menu.')
+    expect(menuItemRepository.items).toHaveLength(0)
+    expect(menuItemRepository.callsCount).toBe(0)
+    expect(menuSectionRepository.items).toHaveLength(0)
+    expect(menuSectionRepository.addCallsCount).toBe(0)
+    expect(menuSectionRepository.getMenuCallsCount).toBe(1)
   })
 })
